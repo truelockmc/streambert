616971f58568b19e9b4f44b01669bec1e2cf4734
true_lock <168660625+truelockmc@users.noreply.github.com>
2026-02-24 16:36:07 +0100

    fixed #7

--- src/utils/api.js
+++ src/utils/api.js
@@ -66,9 +66,9 @@ export const PLAYER_SOURCES = [
     tag: null,
     note: null,
     supportsProgress: false,
-    movieUrl: (id) => `https://www.2embed.cc/embed/${id}`,
+    movieUrl: (id) => `https://www.2embed.stream/embed/movie/${id}`,
     tvUrl: (id, season, ep) =>
-      `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${ep}`,
+      `https://www.2embed.stream/embed/tv/${id}/${season}/${ep}`,
   },
   {
     id: "allmanga",
